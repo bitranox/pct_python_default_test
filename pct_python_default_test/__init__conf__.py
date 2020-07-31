555cbc4e60cf4d070969f196c578af65c3182db4
bitranox <rnowotny1966@gmail.com>
2020-07-31 11:00:34 +0200

    refractor

--- pct_python_default_test/__init__conf__.py
+++ pct_python_default_test/__init__conf__.py
@@ -1,6 +1,6 @@
 name = 'pct_python_default_test'
 title = 'a pizzacutter default test project, crated with PizzaCutter and the PizzaCutter default python template'
-version = '0.1.0'
+version = '0.0.1'
 url = 'https://github.com/bitranox/pct_python_default_test'
 author = 'Robert Nowotny'
 author_email = 'bitranox@gmail.com'
@@ -14,7 +14,7 @@ Info for pct_python_default_test:
 
     a pizzacutter default test project, crated with PizzaCutter and the PizzaCutter default python template
 
-    Version : 0.1.0
+    Version : 0.0.1
     Url     : https://github.com/bitranox/pct_python_default_test
     Author  : Robert Nowotny
     Email   : bitranox@gmail.com""")
